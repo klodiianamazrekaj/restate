beeb57e49f8ac498ffef2cf4eea5bfa9b1cb7f50
Klodiana Mazrekaj <klodianamazrekaj20@gmail.com>
2025-01-03 17:10:10 +0100

    setup nativewind

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -11,6 +11,7 @@ export default function Index() {
       }}
     >
 
+      <Text className="font-bold text-lg my-10">Welcome to ReState</Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
